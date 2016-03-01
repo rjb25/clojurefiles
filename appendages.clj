703bc057bb34652cc5b08354472a58c4ef0771de
Jason <rbasanes@emich.edu>
2016-03-01 18:24:36 -0500

    removed uneccessary line

--- appendages.clj
+++ appendages.clj
@@ -1,6 +1,5 @@
 (def next-apendage 
   #(hash-map :name (clojure.string/replace (:name %1) #"[0-9]" (str (+ 1 %2 (read-string (re-find #"[0-9]" (:name %1))))))))
-(hash-map :name "hello")
 
 (defn add-apendages
   [times part]
