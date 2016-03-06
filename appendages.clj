a6ed7b23feaefc2f1ef76820d48b1ec277f691f2
Jason <rbasanes@emich.edu>
2016-03-05 19:18:58 -0500

    improved commentary on appendages

--- appendages.clj
+++ appendages.clj
@@ -1,15 +1,18 @@
-(def next-apendage 
-  #(hash-map :name (clojure.string/replace (:name %1) #"[0-9]" (str (+ 1 %2 (read-string (re-find #"[0-9]" (:name %1))))))))
+;;Program for adding limbs to a map of body parts
+(comment "Defines a function that gives the next limb")
+(def next-appendage #(hash-map :name (clojure.string/replace (:name %1) #"[0-9]" (str (+ 1 %2 (read-string (re-find #"[0-9]" (:name %1))))))))
+(next-appendage {:name "arm-1"} 0);;=> {:name "arm-2"}
 
-(defn add-apendages
+(defn add-appendages
+  "Adds given ammount of appendages"
   [times part]
   (loop [done 0
-         final-apendages [part]]
+         final-appendages [part]]
     (if (= done times)
-      final-apendages 
+      final-appendages 
       (recur 
        (inc done) 
-       (conj final-apendages
-             (next-apendage part done))))))
+       (conj final-appendages
+             (next-appendage part done))))))
   
-(add-apendages 2 {:name "tentacle-3"}) ;;OUTPUT->[{:name "tentacle-3"} {:name "tentacle-4"} {:name "tentacle-5"}]
+(add-appendages 2 {:name "tentacle-3"}) ;;OUTPUT->[{:name "tentacle-3"} {:name "tentacle-4"} {:name "tentacle-5"}]
