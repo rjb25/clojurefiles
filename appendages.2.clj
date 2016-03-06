6193c633a4f591b3ccebe8e7bdee3d7e61701fa7
Jason <rbasanes@emich.edu>
2016-03-05 19:26:01 -0500

    latest commentary

--- appendages.2.clj
+++ appendages.2.clj
@@ -1,4 +1,5 @@
 ;;Appendages approved by way of code review: http://codereview.stackexchange.com/questions/121621/adding-augmented-maps-to-a-vector-based-on-regex-text-grabbing
+;;Program for adding limbs to a map of body parts
 
 (ns appendages.core
   (:require (clojure [string :as s])))
@@ -11,8 +12,9 @@
                 (+ 1 done)
                 str
                 (s/replace % #"[0-9]"))))
+(next-appendage {:name "arm-1"} 1);;=> {:name "arm-2"}
 
 (defn add-appendages
   [times part]
   (cons part (map (partial next-appendage part) (range times))))
-(add-appendages 2 {:name "tentacle-3"}) ;;OUTPUT->[{:name "tentacle-3"} {:name "tentacle-4"} {:name "tentacle-5}]
+(add-appendages 2 {:name "tentacle-3"}) ;;=>({:name "tentacle-3"} {:name "tentacle-4"} {:name "tentacle-5})
